640f72423ba3f1d1d31ad7779249e0005d3fd24a
Thomas Frengler <thomasfrengler@gmail.com>
2024-01-27 21:06:27 +0100

    Getting started

---
+++ 2023/Day01.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2023._2023
+{
+    [TestFixture]
+    public class Day01
+    {
+        [TestCase]
+        public void Part01()
+        {
+
+        }
+
+        [TestCase]
+        public void Part02()
+        {
+
+        }
+    }
+}
