469297a2d46d1cecfd3c81ac7b0c08e0ec81d0c4
Thomas Frengler <thomasfrengler@gmail.com>
2024-01-27 20:13:21 +0100

    FIRST

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode2023
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
