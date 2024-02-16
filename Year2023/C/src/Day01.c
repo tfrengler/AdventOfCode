c959057ddec2e32a5e7f891bd1451ac2b92f6755
Thomas Frengler <thomasfrengler@gmail.com>
2024-02-16 18:02:35 +0100

    First day implemented, yet we get the wrong answer...

--- Year2023/C/src/Day01.c
+++ Year2023/C/src/Day01.c
@@ -1,21 +1,45 @@
 #include "LibThomas.h"
 #include "stdlib.h"
+#include <assert.h>
 #include <strings.h>
+#include "stdio.h"
 
 int main(void)
 {
-    StringArray* Input = File_ReadAllLines("src/Debug.txt");
+    StringArray* Input = File_ReadAllLines("Input/01.txt");
+    i32 PartOneAnswer = 0;
 
-    char Buffer[10] = {0};
+    char Buffer[3];
+    memset(&Buffer, -1, 2);
+    Buffer[2] = '\0';
+    int BufferIndex = 0;
 
-    for(i32 Index = 0; Index < Input->Count; Index++)
+    for(i32 LineIndex = 0; LineIndex < Input->Count; LineIndex++)
     {
-        String Current = *Input->Contents[Index];
-        // loop through string
-        // check if char is digit
-        // add to buffer if yes
-        memset(&Buffer, 0, 10);
+        String* CurrentString = Input->Contents[LineIndex];
+
+        for(i32 StringIndex = 0; StringIndex < CurrentString->Size; StringIndex++)
+        {
+            char CurrentChar = CurrentString->Content[StringIndex];
+            if ((int)CurrentChar < 58 && (int)CurrentChar > 48)
+            {
+                Buffer[BufferIndex] = CurrentChar;
+                if (BufferIndex == 0) BufferIndex = 1;
+            }
+        }
+
+        i32 CalibrationNumber = atoi(Buffer);
+        //printf("CalibrationNumber: %i\n", CalibrationNumber);
+
+        memset(&Buffer, -1, 2);
+        BufferIndex = 0;
+
+        PartOneAnswer += CalibrationNumber;
+        //if (LineIndex == 10) break;
     }
+
+    printf("Part one answer: %i\n", PartOneAnswer);
+    assert(PartOneAnswer == 55621);
 
     return EXIT_SUCCESS;
 }
