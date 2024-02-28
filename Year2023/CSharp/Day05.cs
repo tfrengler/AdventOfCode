d68a702ab707ca8c1bb103bdfbc519f2e88351fb
Thomas Frengler <thomas@gmail.com>
2024-02-28 17:13:59 +0100

    C# - First attempt at part 2. No solution yet because runtime is so damn long... Need to figure out how to optimize this.

--- Year2023/CSharp/Day05.cs
+++ Year2023/CSharp/Day05.cs
@@ -8,19 +8,31 @@ using System.Text.RegularExpressions;
 
 namespace AdventOfCode2023.Year2023
 {
+    [TestFixture]
+    [NonParallelizable]
     public class Day05
     {
         readonly string[] Input;
 
+        Regex DigitRegex = new Regex(@"(\d+)");
+
+        long[] Seeds;
+
+        List<ValueTuple<long, long, long>> SeedToSoilMap;
+        List<ValueTuple<long, long, long>> SoilToFertilizerMap;
+        List<ValueTuple<long, long, long>> FertilizerToWaterMap;
+        List<ValueTuple<long, long, long>> WaterToLightMap;
+        List<ValueTuple<long, long, long>> LightToTemperatureMap;
+        List<ValueTuple<long, long, long>> TemperatureToHumidityMap;
+        List<ValueTuple<long, long, long>> HumidityToLocationMap;
+
+        List<ValueTuple<long, long, long>>[] RefsToMapsInOrder;
+
         public Day05()
         {
             Input = File.ReadAllLines("Input/05.txt");
-        }
 
-        [TestCase]
-        public void Part01()
-        {
-            /*string[] Input = new[]
+            /*Input = new[]
             {
                 "seeds: 79 14 55 13",
                 "",
@@ -56,25 +68,44 @@ namespace AdventOfCode2023.Year2023
                 "60 56 37",
                 "56 93 4"
             };*/
+        }
 
-            var DigitRegex = new Regex(@"(\d+)");
+        static long CalculateDestinationIndex(in List<ValueTuple<long, long, long>> map, long input)
+        {
+            long ReturnData = 0;
 
-            long[] Seeds = null;
+            foreach (var (dest, source, range) in map)
+            {
+                if (input >= source && input < (source + range))
+                {
+                    ReturnData = (dest - source) + input;
+                    break;
+                }
+                else
+                {
+                    ReturnData = input;
+                }
+            }
+            return ReturnData;
+        }
 
-            var SeedToSoilMap = new List<ValueTuple<long,long,long>>();
-            var SoilToFertilizerMap = new List<ValueTuple<long,long,long>>();
-            var FertilizerToWaterMap = new List<ValueTuple<long,long,long>>();
-            var WaterToLightMap = new List<ValueTuple<long,long,long>>();
-            var LightToTemperatureMap = new List<ValueTuple<long,long,long>>();
-            var TemperatureToHumidityMap = new List<ValueTuple<long,long,long>>();
-            var HumidityToLocationMap = new List<ValueTuple<long,long,long>>();
-
-            List<ValueTuple<long,long,long>> CurrentMap = null;
-
+        void ParseSeeds()
+        {
             var DigitMatches = DigitRegex.Matches(Input[0]);
             Seeds = DigitMatches.Select(x => x.Groups[0].Value).Select(x => Convert.ToInt64(x)).ToArray();
+        }
 
-            // Parsing
+        void ParseMaps()
+        {
+            SeedToSoilMap = new List<ValueTuple<long, long, long>>();
+            SoilToFertilizerMap = new List<ValueTuple<long, long, long>>();
+            FertilizerToWaterMap = new List<ValueTuple<long, long, long>>();
+            WaterToLightMap = new List<ValueTuple<long, long, long>>();
+            LightToTemperatureMap = new List<ValueTuple<long, long, long>>();
+            TemperatureToHumidityMap = new List<ValueTuple<long, long, long>>();
+            HumidityToLocationMap = new List<ValueTuple<long, long, long>>();
+
+            List<ValueTuple<long, long, long>>? CurrentMap = null;
             for (int index = 1; index < Input.Length; index++)
             {
                 string currentLine = Input[index];
@@ -100,6 +131,8 @@ namespace AdventOfCode2023.Year2023
                     continue;
                 }
 
+                Trace.Assert(CurrentMap is not null);
+
                 var Parts = currentLine.Split(' ');
                 Trace.Assert(Parts.Length == 3);
 
@@ -108,7 +141,45 @@ namespace AdventOfCode2023.Year2023
                     Convert.ToInt64(Parts[1]),
                     Convert.ToInt64(Parts[2]))
                 );
+
+                RefsToMapsInOrder = new[]
+                {
+                    SeedToSoilMap,
+                    SoilToFertilizerMap,
+                    FertilizerToWaterMap,
+                    WaterToLightMap,
+                    LightToTemperatureMap,
+                    TemperatureToHumidityMap,
+                    HumidityToLocationMap
+                };
             }
+        }
+
+        long[] ParseAndCalculateLocations(IList<long> seeds)
+        {
+            long[] Locations = new long[seeds.Count];
+
+            for (int SeedIndex = 0; SeedIndex < seeds.Count; SeedIndex++)
+            {
+                long DestinationIndex = seeds[SeedIndex];
+
+                for (int ChainIndex = 0; ChainIndex < RefsToMapsInOrder.Length; ChainIndex++)
+                {
+                    DestinationIndex = CalculateDestinationIndex(RefsToMapsInOrder[ChainIndex], DestinationIndex);
+                }
+
+                Locations[SeedIndex] = DestinationIndex;
+            }
+
+            return Locations;
+        }
+
+        [TestCase]
+        public void Part01()
+        {
+            ParseSeeds();
+            ParseMaps();
+
             /*
             Debug.Assert(Seeds.Length == 4);
             Debug.Assert(SeedToSoilMap.Count == 2, string.Join(Environment.NewLine, SeedToSoilMap));
@@ -131,53 +202,41 @@ namespace AdventOfCode2023.Year2023
             Expected soil results: 81, 14, 57, 13
             */
 
-            static long CalculateDestinationIndex(List<ValueTuple<long,long,long>> map, long input)
-            {
-                long ReturnData = 0;
-
-                foreach (var (dest, source, range) in map)
-                {
-                    if (input >= source && input < (source + range))
-                    {
-                        ReturnData = (dest - source) + input;
-                        break;
-                    }
-                    else
-                    {
-                        ReturnData = input;
-                    }
-                }
-                return ReturnData;
-            }
-
-            var RefsToMapsInOrder = new[]
-            {
-                SeedToSoilMap,
-                SoilToFertilizerMap,
-                FertilizerToWaterMap,
-                WaterToLightMap,
-                LightToTemperatureMap,
-                TemperatureToHumidityMap,
-                HumidityToLocationMap
-            };
-
-            Span<long> Locations = stackalloc long[Seeds.Length];
-
-            for (int SeedIndex = 0; SeedIndex < Seeds.Length; SeedIndex++)
-            {
-                long DestinationIndex = Seeds[SeedIndex];
-
-                for (int ChainIndex = 0; ChainIndex < RefsToMapsInOrder.Length; ChainIndex++)
-                {
-                    DestinationIndex = CalculateDestinationIndex(RefsToMapsInOrder[ChainIndex], DestinationIndex);
-                }
-
-                Locations[SeedIndex] = DestinationIndex;
-            }
+            Span<long> Locations = ParseAndCalculateLocations(Seeds);
 
             long FinalValue = Locations.ToArray().Min();
             Console.WriteLine(FinalValue);
             Trace.Assert(FinalValue == 389056265, $"Expected final score to be 389056265 but it was {FinalValue}");
+        }
+
+        [TestCase]
+        public void Part02()
+        {
+            ParseSeeds();
+            ParseMaps();
+
+            List<long> ParsedSeedsBasedOnRange = new List<long>();
+            long FinalValue = long.MaxValue;
+
+            for (int Index = 0; Index < Seeds.Length; Index += 2)
+            {
+                long SeedStart = Seeds[Index];
+                long SeedRange = Seeds[Index + 1];
+
+                for(long RangeIndex = 0; RangeIndex < SeedRange; RangeIndex++)
+                {
+                    ParsedSeedsBasedOnRange.Add(SeedStart + RangeIndex);
+                }
+
+                long[] Locations = ParseAndCalculateLocations(ParsedSeedsBasedOnRange);
+                ParsedSeedsBasedOnRange.Clear();
+                long MinValue = Locations.Min();
+
+                FinalValue = MinValue < FinalValue ? MinValue : FinalValue;
+            }
+
+            Console.WriteLine(FinalValue);
+            //Trace.Assert(FinalValue == 46, $"Expected final score to be 46 but it was {FinalValue}");
         }
     }
 }
