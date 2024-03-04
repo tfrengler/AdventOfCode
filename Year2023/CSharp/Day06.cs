94bb8f21d62a94baf1985993bb85c081116394b2
Thomas Frengler <thomasfrengler@gmail.com>
2024-03-04 14:24:37 +0100

    C# - Solved day 6 part 1 and 2

---
+++ Year2023/CSharp/Day06.cs
@@ -0,0 +1,79 @@
+﻿using NUnit.Framework;
+using System;
+using System.Diagnostics;
+
+namespace AdventOfCode2023.Year2023
+{
+    [TestFixture]
+    [NonParallelizable]
+    public class Day06
+    {
+        private readonly struct Race
+        {
+            public long TimeInMs { get; }
+            public long BestDistance { get; }
+
+            public Race(long time, long distance)
+            {
+                TimeInMs = time;
+                BestDistance = distance;
+            }
+        }
+
+        [TestCase]
+        public void Part01()
+        {
+            var Input = new Race[]
+            {
+                new(56, 499),
+                new(97, 2210),
+                new(77, 1097),
+                new(93, 1440)
+            };
+
+            int FinalAnswer = 1;
+
+            foreach (Race CurrentRace in Input)
+            {
+                int WinningCombos = 0;
+
+                for (int IndexMiliseconds = 1; IndexMiliseconds < CurrentRace.TimeInMs; IndexMiliseconds++)
+                {
+                    // (7 - 2) = (5 * 2) = (10 > 9) == True
+                    long DistanceDone = (CurrentRace.TimeInMs - IndexMiliseconds) * IndexMiliseconds;
+                    if (DistanceDone > CurrentRace.BestDistance)
+                    {
+                        WinningCombos++;
+                    }
+                }
+                
+                FinalAnswer *= WinningCombos;
+            }
+
+            Console.WriteLine("Part one answer: " + FinalAnswer);
+            Trace.Assert(FinalAnswer == 1710720, $"Expected final answer to be 1710720 but it was {FinalAnswer}");
+        }
+
+        [TestCase]
+        public void Part02()
+        {
+            long FinalAnswer;
+            int WinningCombos = 0;
+            Race CurrentRace = new(56977793, 499221010971440);
+
+            for (int IndexMiliseconds = 1; IndexMiliseconds < CurrentRace.TimeInMs; IndexMiliseconds++)
+            {
+                long DistanceDone = (CurrentRace.TimeInMs - IndexMiliseconds) * IndexMiliseconds;
+                if (DistanceDone > CurrentRace.BestDistance)
+                {
+                    WinningCombos++;
+                }
+            }
+
+            FinalAnswer = WinningCombos;
+
+            Console.WriteLine("Part two answer: " + FinalAnswer);
+            Trace.Assert(FinalAnswer == 35349468, $"Expected final answer to be 35349468 but it was {FinalAnswer}");
+        }
+    }
+}
---
+++ Year2023/CSharp/Day07.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace AdventOfCode2023.Year2023
+{
+    // https://www.reddit.com/r/adventofcode/comments/18cq5j3/2023_day_7_part_1_two_hints_for_anyone_stuck/
+
+    [TestFixture]
+    [NonParallelizable]
+    public sealed class Day07
+    {
+        [TestCase]
+        public void Part01()
+        {
+
+        }
+
+        [TestCase]
+        public void Part02()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
