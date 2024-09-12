1bd84a9eba25473111843dd82b48826a9510d280
Thomas Frengler <thomas@gmail.com>
2024-09-12 14:50:37 +0200

    C - Day 8 part 1 done

--- Year2023/C/src/Day08.c
+++ Year2023/C/src/Day08.c
@@ -5,28 +5,10 @@
 #include <string.h>
 
 typedef struct _Node {
-    struct _Node *Left;
-    struct _Node *Right;
-    // char Name[3];
+    char *Name;
+    char *Left;
+    char *Right;
 } Node;
-
-void ParseInput(StringArray *input)
-{
-    Node **Nodes = malloc(sizeof(Node) * 1000000);
-    Node *Test = malloc(sizeof(Node));
-    Nodes[0] = Test;
-}
-
-// uint64_t hash(char *str)
-// {
-//     uint64_t hash = 5381;
-//     int c;
-
-//     while ((c = *str++))
-//         hash = ((hash << 5) + hash) + c;
-
-//     return hash;
-// }
 
 int main(void)
 {
@@ -37,15 +19,75 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
-    // ParseInput(TestInput);
+    #define AmountOfNodes() 762
+    const int32_t AmountOfNodes = AmountOfNodes();
+    int32_t NodeInsertIndex = 0;
+    Node Nodes[AmountOfNodes()] = { 0 };
+    String *Instructions = TestInput->Contents[0];
+    int32_t StartNodeIndex = 264;
 
+    // LDN = (BRL, FLC)
     for (int32_t index = 2; index < TestInput->Count; index++) {
 
-        char Buffer[4] = { 0 };
-        memcpy(Buffer, TestInput->Contents[index]->Content, 3);
-        int NodeAsInt = 0;
+        char *Name = malloc(4 * sizeof(char));
+        char *Left = malloc(4 * sizeof(char));
+        char *Right = malloc(4 * sizeof(char));
+        Name[3] = 0x0;
+        Left[3] = 0x0;
+        Right[3] = 0x0;
 
-        printf("%s : %i%i%i\n", Buffer, (int)Buffer[0], (int)Buffer[1], (int)Buffer[2]);
+        char *Source = TestInput->Contents[index]->Content;
+        memcpy(Name, Source, 3);
+        memcpy(Left, &Source[7], 3);
+        memcpy(Right, &Source[12], 3);
+
+        Node NewNode = {
+            .Name = Name,
+            .Left = Left,
+            .Right = Right
+        };
+        Nodes[NodeInsertIndex] = NewNode;
+        NodeInsertIndex++;
+    }
+
+    int32_t NextNodeIndex = StartNodeIndex;
+    int32_t NextInstructionIndex = 0;
+    Part1Answer++;
+
+    while(true) {
+
+        Node NextNode = Nodes[NextNodeIndex];
+        char *NextNodeName = 0x0;
+        char NextInstruction = Instructions->Content[NextInstructionIndex];
+
+        if (NextInstruction == 'L') {
+            NextNodeName = NextNode.Left;
+        }
+        else if (NextInstruction == 'R') {
+            NextNodeName = NextNode.Right;
+        } else {
+            // DEBUG_PRINT("NextInstruction is not L or R", 0x0);
+            exit(EXIT_FAILURE);
+        }
+
+        if (strcmp(NextNodeName, "ZZZ") == 0) {
+            break;
+        }
+
+        for (int32_t NodeSearchIndex = 0; NodeSearchIndex < (AmountOfNodes - 1); NodeSearchIndex++) {
+            if (strcmp(NextNodeName, Nodes[NodeSearchIndex].Name) == 0) {
+                NextNodeIndex = NodeSearchIndex;
+                break;
+            }
+        }
+
+        Part1Answer++;
+
+        if (NextInstructionIndex == (Instructions->Size - 1)) {
+            NextInstructionIndex = 0;
+        } else {
+            NextInstructionIndex++;
+        }
     }
 
     printf("Part 1 answer: %i\n", Part1Answer);
