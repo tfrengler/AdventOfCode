76cb4270b2583de281626746653c63fe10d2cfc1
Thomas Frengler <thomas@gmail.com>
2024-09-18 16:59:56 +0200

    C# - debugging hash table

--- Year2023/CSharp/Debuggery.cs
+++ Year2023/CSharp/Debuggery.cs
@@ -11,28 +11,27 @@ namespace AdventOfCode.Year2023
     [TestFixture]
     public sealed class Debuggery
     {
-        private readonly Dictionary<string,Tuple<string,string>> Nodes;
+        private readonly Dictionary<string,string> Nodes;
 
         public Debuggery()
         {
-            string[] Input = File.ReadAllLines("./Input/08.txt");
-            Nodes = new(762);
+            string[] Input = File.ReadAllLines("./Input/Debug.txt");
+            Nodes = new(18);
 
             int IndicesIndex = 0;
-            for (int Index = 2; Index < Input.Length; Index++)
+            for (int Index = 0; Index < Input.Length; Index++)
             {
                 string CurrentLine = Input[Index];
 
-                string MapIndex = CurrentLine.Substring(0, 3);
-                string MapValueL = CurrentLine.Substring(7, 3);
-                string MapValueR = CurrentLine.Substring(12, 3);
+                string MapIndex = CurrentLine[..3];
+                string MapValue = CurrentLine[6..12];
 
-                Nodes[MapIndex] = new Tuple<string, string>(MapValueL, MapValueR);
+                Nodes[MapIndex] = MapValue;
                 IndicesIndex++;
             }
         }
 
-        public static uint Fnv1aHash(string input, uint offsetBasis = 2166136261)
+        public static uint Fnv1aHash(string input, uint offsetBasis = 0)
         {
             return Fnv1aHash(Encoding.ASCII.GetBytes(input), offsetBasis);
         }
@@ -68,8 +67,8 @@ namespace AdventOfCode.Year2023
 
             uint Size = (uint)Nodes.Count;
             var Buckets = new List<string>[Size];
-            var Intermediate = new long[Size];
-            var Values = new Tuple<string,string>[Size];
+            var IntermediateTable = new long[Size];
+            var ValueTable = new string[Size];
 
             // Create temp buckets to group the hashed values together in
             for(int Index = 0; Index < Buckets.Length; Index++)
@@ -94,7 +93,7 @@ namespace AdventOfCode.Year2023
                 return 0;
             });
 
-            /*using (var BucketFile = File.Create("C:/Temp/Buckets.txt"))
+            using (var BucketFile = File.Create("C:/Temp/Buckets.txt"))
             {
                 for (int Index = 0; Index < Buckets.Length; Index++)
                 {
@@ -102,7 +101,7 @@ namespace AdventOfCode.Year2023
                     //Console.WriteLine($"Count ({Index}): " + Buckets[Index].Count);
                 }
                 BucketFile.Flush();
-            }*/
+            }
 
             //return;
             /* STEP 2: Parse buckets with multiple entries (duplicates), create and populate the intermediate- and final value table */
@@ -116,7 +115,8 @@ namespace AdventOfCode.Year2023
                     break;
                 }
 
-                uint HashOffsetBasis = DefaultOffsetBasis;
+                // Start with +1 because we already know these values collide with the default
+                uint HashOffsetBasis = DefaultOffsetBasis+1;
                 var BucketIndex = 0;
                 int Iterations = 0;
                 // Temp table containing all the computed indices
@@ -132,7 +132,7 @@ namespace AdventOfCode.Year2023
                     // Compute index in final value table (hash-algorithm mod final value table size)
                     uint ValueSlot = Fnv1aHash(CurrentKey, HashOffsetBasis) % Size;
                     // Check if final value table index we computed is occupoed OR temp index table contains an already computed index
-                    if (Values[ValueSlot] is not null || SlotsInValueTable.Contains(ValueSlot))
+                    if (ValueTable[ValueSlot] is not null || SlotsInValueTable.Contains(ValueSlot))
                     {
                         // We have collision. Reset and retry entire bucket with different offset
                         HashOffsetBasis++;
@@ -161,24 +161,24 @@ namespace AdventOfCode.Year2023
                         => index in intermediate table
                         => hashoffset to use for value to get index for value in final value table
                 */
-                Intermediate[Fnv1aHash(CurrentBucket[0]) % Size] = HashOffsetBasis;
+                IntermediateTable[Fnv1aHash(CurrentBucket[0]) % Size] = HashOffsetBasis;
 
                 // Loop through values in bucket and insert them into final value table based on indices we computed and stored in temp table
                 for(int IndexInner = 0; IndexInner < CurrentBucket.Count; IndexInner++)
                 {
-                    Debug.Assert(Values[SlotsInValueTable[IndexInner]] == null, $"Expected Values[{SlotsInValueTable[IndexInner]}] to be null");
-                    Values[SlotsInValueTable[IndexInner]] = Nodes[CurrentBucket[IndexInner]];
+                    Debug.Assert(ValueTable[SlotsInValueTable[IndexInner]] == null, $"Expected Values[{SlotsInValueTable[IndexInner]}] to be null");
+                    ValueTable[SlotsInValueTable[IndexInner]] = Nodes[CurrentBucket[IndexInner]];
                 }
             }
 
             /* STEP 3: Parse the remaining values that hashed to a unique value without duplicates and fit them into the holes in the table */
 
             // Iterate over the final value table and find empty slots
-            int FreeSlotIndex = 0;
+            /*int FreeSlotIndex = 0;
             var FreeSlotsList = new int[Size - (BucketsWithOneStartIndex + 1)];
             for (int Index = 0; Index < Size; Index++)
             {
-                if (Values[Index] is null)
+                if (ValueTable[Index] is null)
                 {
                     FreeSlotsList[FreeSlotIndex] = Index;
                     FreeSlotIndex++;
@@ -201,9 +201,9 @@ namespace AdventOfCode.Year2023
                 // Set the intermediate table value to the index in the value table.
                 // However to indicate that it's a direct reference make it negative.
                 // We subtract 1 to cover the case where the value table index is 0.
-                Intermediate[IntermediateSlot] = (-ValueSlot) - 1;
-                Values[ValueSlot] = Nodes[CurrentBucket[0]];
-            }
+                IntermediateTable[IntermediateSlot] = (-ValueSlot) - 1;
+                ValueTable[ValueSlot] = Nodes[CurrentBucket[0]];
+            }*/
 
             /*
              NOTE: An optimization for space/memory is to compress the intermediate table ie. removing the empty cells
@@ -212,17 +212,17 @@ namespace AdventOfCode.Year2023
 
             using (var OutputFile = File.Create("C:/Temp/IntermediateTable.txt"))
             {
-                for (int Index = 0; Index < Intermediate.Length; Index++)
+                for (int Index = 0; Index < IntermediateTable.Length; Index++)
                 {
-                    OutputFile.Write(Encoding.UTF8.GetBytes($"{Index}: " + Intermediate[Index] + Environment.NewLine));
+                    OutputFile.Write(Encoding.UTF8.GetBytes($"{Index}: " + IntermediateTable[Index] + Environment.NewLine));
                 }
                 OutputFile.Flush();
             }
             using (var OutputFile = File.Create("C:/Temp/ValueTable.txt"))
             {
-                for (int Index = 0; Index < Values.Length; Index++)
+                for (int Index = 0; Index < ValueTable.Length; Index++)
                 {
-                    OutputFile.Write(Encoding.UTF8.GetBytes($"{Index}: " + Values[Index] + Environment.NewLine));
+                    OutputFile.Write(Encoding.UTF8.GetBytes($"{Index}: " + ValueTable[Index] + Environment.NewLine));
                 }
                 OutputFile.Flush();
             }
@@ -230,75 +230,27 @@ namespace AdventOfCode.Year2023
             // TESTING AND VALIDATION
             foreach (var currentNode in Nodes)
             {
-                long HashOffset = Intermediate[Fnv1aHash(currentNode.Key) % Size];
+                long HashOffset = IntermediateTable[Fnv1aHash(currentNode.Key) % Size];
                 if (HashOffset == 0)
                 {
-                    throw new Exception($"Key {currentNode.Key} does not exist in hash table");
+                    continue;
+                    //throw new Exception($"Key {currentNode.Key} does not exist in hash table");
                 }
 
-                Tuple<string, string> Value;
+                string Value = string.Empty;
                 if (HashOffset > 0)
                 {
-                    Value = Values[Fnv1aHash(currentNode.Key, (uint)HashOffset) % Size];
+                    Value = ValueTable[Fnv1aHash(currentNode.Key, (uint)HashOffset) % Size];
                 }
-                else
-                {
-                    long ValueIndex = (-HashOffset) + 1;
-                    Value = Values[ValueIndex];
-                }
+                //else
+                //{
+                //    long ValueIndex = (-HashOffset) + 1;
+                //    Value = ValueTable[ValueIndex];
+                //}
 
                 Debug.Assert(currentNode.Value == Value, $"Expected key '{currentNode.Key}' to yield value '{currentNode.Value}' but found '{Value}'");
                 //Console.WriteLine($"{currentNode.Key} = ({currentNode.Value.Item1},{currentNode.Value.Item2}) | ({Value.Item1},{Value.Item2})");
             }
-        }
-
-        [TestCase]
-        public void DoDebug1()
-        {
-            //uint[]? Primes = JsonSerializer.Deserialize<uint[]>(File.ReadAllText("./Input/10kPrimes.json"));
-            //System.Diagnostics.Debug.Assert(Primes is not null);
-            /*
-            var Map = new string[762];
-            bool Collided = false;
-
-            for (uint Index = 0; Index < 10_000_000; Index++)
-            {
-                foreach (string current in Indices)
-                {
-                    //string NumberString = $"{(int)current[0]}{(int)current[1]}{(int)current[2]}";
-                    //uint AsInt1 = Convert.ToUInt32(NumberString);
-
-                    int AsInt = current[0] * 100;
-                    AsInt = AsInt + current[1];
-                    AsInt = (AsInt * 100) + current[2];
-
-                    uint Hash = Fnv1aHash((uint)AsInt + Index);
-                    uint HashIndex = Hash % 762;
-
-                    if (Map[HashIndex] != null)
-                    {
-                        Collided = true;
-                        break;
-                    }
-
-                    Map[HashIndex] = current;
-                }
-
-                if (!Collided)
-                {
-                    Console.WriteLine($"No collision! " + Index);
-                    for(int MapIndex = 0; MapIndex < Map.Length; MapIndex++)
-                    {
-                        Console.WriteLine($"{MapIndex} : {Map[MapIndex]}");
-                    }
-                    return;
-                }
-
-                Collided = false;
-                Map = new string[762];
-            }
-
-            Console.WriteLine($"None worked :(");*/
         }
     }
 }
