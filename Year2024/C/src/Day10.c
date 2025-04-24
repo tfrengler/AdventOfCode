b94e8b7bbfc238025517d7b16c83a8fd3ce9b1f1
Thomas Frengler <thomas.frengler@cavero.nl>
2025-04-24 21:39:08 +0200

    C - Day 10 part 1 and 2 done

--- Year2024/C/src/Day10.c
+++ Year2024/C/src/Day10.c
@@ -2,57 +2,90 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "LibGrid.h"
 #include "LibMem.h"
 #include "LibThomas.h"
 #include "LibString.h"
-// #include "LibNumeric.h"
 
-typedef struct _Trailhead
+typedef struct _TrailPosition
 {
     int32_t Row;
     int32_t Column;
     int32_t Number;
-} Trailhead;
+} TrailPosition;
+
+#define VISITED_BUFFER_COUNT() 500
 
 StringArray *Input = nullptr;
 
+int32_t VisitedIndex = 0;
+TrailPosition Visited[VISITED_BUFFER_COUNT()] = {0};
+
+size_t TrailheadIndex = 0;
+size_t TrailheadCount = 0;
+TrailPosition TrailHeads[275] = {0}; // Cheating a bit by knowing how many trailheads are there beforehand
+
 void Setup(void)
 {
-    MemArena_Init(1024 * 1024 * 4); // 4mb heap
-    // Input = File_ReadAllText("./Input/10.txt");
-
-    Input = File_ReadAllLines("./Input/Debug.txt");
+    MemArena_Init(1024 * 1024 * 1);
+    Input = File_ReadAllLines("./Input/10.txt");
     assert(Input != nullptr);
 
     Grid_BoundaryCrossIsNotFatal();
+
+    for(int32_t rowIndex = 0; rowIndex < Input->Count; rowIndex++) {
+        String *CurrentRow = Input->Contents[rowIndex];
+
+        for(int32_t columnIndex = 0; columnIndex < Input->Count; columnIndex++) {
+
+            if (CurrentRow->Content[columnIndex] == '0') {
+                TrailHeads[TrailheadIndex] = (TrailPosition){ .Row = rowIndex, .Column = columnIndex };
+                TrailheadIndex++;
+            }
+        }
+    }
+
+    TrailheadCount = TrailheadIndex;
 }
 
-void EnumerateTrail(Trailhead trailhead, int32_t *score) {
+void EnumerateTrail(TrailPosition position, bool checkVisited, int32_t *score) {
 
-    GridPoint TrailPoints[4] = {0};
+    if (position.Number == 9) {
+        if (checkVisited) {
+            for(int32_t visitIndex = 0; visitIndex < VisitedIndex; visitIndex++) {
+                if (Visited[visitIndex].Row == position.Row && Visited[visitIndex].Column == position.Column) {
+                    return;
+                }
+            }
+
+            Visited[VisitedIndex] = position;
+            VisitedIndex++;
+        }
+
+        (*score)++;
+        return;
+    }
+
+    GridPoint AdjacentPositions[4] = {0};
     Grid_GetCross(
         Input,
-        TrailPoints,
-        trailhead.Column,
-        trailhead.Row);
+        AdjacentPositions,
+        position.Column,
+        position.Row);
 
     for(int32_t index = 0; index < 4; index++) {
-        if (!TrailPoints[index].IsValid) continue;
+        GridPoint *CurrentAdjacentPosition = &AdjacentPositions[index];
+        if (!CurrentAdjacentPosition->IsValid) continue;
 
-        int32_t NextNumber = TrailPoints[index].Value - '0';
-        if (trailhead.Number == 8 && NextNumber == 9) {
-            // Is a full trail
-            (*score)++;
-            return;
+        int32_t AdjacentPositionValue = CurrentAdjacentPosition->Value - '0';
+
+        if (AdjacentPositionValue - position.Number != 1) {
+            continue;
         }
 
-        if (NextNumber - trailhead.Number == 1) {
-            EnumerateTrail(
-                (Trailhead){ .Number = NextNumber, .Row = TrailPoints[index].Y, TrailPoints[index].X },
-                score
-            );
-        }
+        TrailPosition NextPosition = (TrailPosition){ .Number = AdjacentPositionValue, .Row = AdjacentPositions[index].Y, AdjacentPositions[index].X };
+        EnumerateTrail(NextPosition, checkVisited, score);
     }
 }
 
@@ -61,41 +94,21 @@ void Part01(void)
     int64_t PartAnswer = 0;
     TimerStart();
 
-    size_t TrailHeadIndex = 0;
-    size_t TrailheadCount = 0;
-    // Trailhead TrailHeads[275] = {0};
-    Trailhead TrailHeads[9] = {0}; // Cheating a bit by knowing how many trailheads are there beforehand
+    for(TrailheadIndex = 0; TrailheadIndex < TrailheadCount; TrailheadIndex++) {
 
-    for(int32_t rowIndex = 0; rowIndex < Input->Count; rowIndex++) {
-        String *CurrentRow = Input->Contents[rowIndex];
-
-        for(int32_t columnIndex = 0; columnIndex < Input->Count; columnIndex++) {
-
-            if (CurrentRow->Content[columnIndex] == '0') {
-                TrailHeads[TrailHeadIndex] = (Trailhead){ .Row = rowIndex, .Column = columnIndex };
-                TrailHeadIndex++;
-            }
-        }
-    }
-
-    TrailheadCount = TrailHeadIndex;
-    TrailHeadIndex = 0;
-
-    for(; TrailHeadIndex < TrailheadCount; TrailHeadIndex++) {
+        memset(Visited, 0, VISITED_BUFFER_COUNT());
+        VisitedIndex = 0;
         int32_t Score = 0;
-        EnumerateTrail(TrailHeads[TrailHeadIndex], &Score);
-        printf("Trailhead %zu score is: %d\n", TrailHeadIndex, Score);
+        EnumerateTrail(TrailHeads[TrailheadIndex], true, &Score);
+
         PartAnswer += Score;
     }
-
-    // int32_t NextNumber = NextCharacter - '0'; // Convert char to int
-    // printf("Trailheads: %zu\n", arrayCount(TrailHeads));
 
     TimerStop();
     PrintTimer();
 
     printf("Part 01 answer: %zu\n", PartAnswer);
-    assert(PartAnswer == 666);
+    assert(PartAnswer == 746);
 }
 
 void Part02(void)
@@ -103,12 +116,21 @@ void Part02(void)
     int64_t PartAnswer = 0;
     TimerStart();
 
+    for(TrailheadIndex = 0; TrailheadIndex < TrailheadCount; TrailheadIndex++) {
+
+        memset(Visited, 0, VISITED_BUFFER_COUNT());
+        VisitedIndex = 0;
+        int32_t Score = 0;
+        EnumerateTrail(TrailHeads[TrailheadIndex], false, &Score);
+
+        PartAnswer += Score;
+    }
 
     TimerStop();
     PrintTimer();
 
     printf("Part 02 answer: %zu\n", PartAnswer);
-    assert(PartAnswer == 6363268339304);
+    assert(PartAnswer == 1541);
 }
 
 int main(void)
@@ -118,6 +140,7 @@ int main(void)
     Part01();
     Part02();
 
+    MemFree(Input);
     // PrintAllocations();
     return EXIT_SUCCESS;
 }
