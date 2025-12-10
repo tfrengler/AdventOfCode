69b39cd5c847685fc17b05bae2f979f6ca314b2b
Thomas Frengler <thomasfrengler@gmail.com>
2025-12-10 16:29:54 +0100

    C#: Day 3, part 1 and 2 solved

---
+++ Year2025/CSharp/Day02.cs
@@ -0,0 +1,27 @@
+﻿using AdventOfCode2025.lib;
+using NUnit.Framework;
+
+namespace AdventOfCode2025
+{
+    [TestFixture]
+    [NonParallelizable]
+    public sealed class Day02 : Day
+    {
+        public Day02(): base(2)
+        {
+            LoadInputAsString();
+        }
+
+        [TestCase]
+        public void Part01()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [TestCase]
+        public void Part02()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Year2025/CSharp/Day03.cs
@@ -0,0 +1,109 @@
+﻿using AdventOfCode2025.lib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace AdventOfCode2025
+{
+    [TestFixture]
+    [NonParallelizable]
+    public sealed class Day03 : Day
+    {
+        public Day03() : base(3)
+        {
+            LoadInputAsLines();
+        }
+
+        [TestCase]
+        public void Part01()
+        {
+            string[] banks = _inputAsLines;
+            //string[] banks = [
+            //    "987654321111111",
+            //    "811111111111119",
+            //    "234234234234278",
+            //    "818181911112111"
+            //];
+
+            int totalOutputJoltage = 0;
+
+            foreach (string bank in banks)
+            {
+                byte finalJoltage = 0;
+                int startIndexSecondDigit = 1;
+
+                foreach (char battery in bank)
+                {
+                    int index = startIndexSecondDigit;
+
+                    while (index < bank.Length)
+                    {
+                        byte newJoltage = Convert.ToByte(new string([battery, bank[index]]));
+                        if (newJoltage > finalJoltage)
+                        {
+                            finalJoltage = newJoltage;
+                        }
+                        index++;
+                    }
+
+                    startIndexSecondDigit++;
+                }
+
+                totalOutputJoltage += finalJoltage;
+            }
+
+            Console.WriteLine("Result: " + totalOutputJoltage);
+            Debug.Assert(totalOutputJoltage == 17524, "Expected result to be 17524");
+        }
+
+        [TestCase]
+        public void Part02()
+        {
+            string[] banks = _inputAsLines;
+            //string[] banks = [
+            //    "987654321111111",
+            //    "811111111111119",
+            //    "234234234234278",
+            //    "818181911112111"
+            //];
+
+            long totalOutputJoltage = 0;
+
+            foreach (string currentBank in banks)
+            {
+                var stack = new Stack<long>();
+                int drops = banks[0].Length - 12;
+
+                long[] batteryJoltages = currentBank.Select(x => Convert.ToInt64(x - '0')).ToArray();
+
+                foreach (long digit in batteryJoltages)
+                {
+                    while (stack.Count > 0 && drops > 0 && stack.Peek() < digit)
+                    {
+                        stack.Pop();
+                        drops--;
+                    }
+
+                    stack.Push(digit);
+                }
+
+                while (drops > 0 && stack.Count > 0)
+                {
+                    stack.Pop();
+                    drops--;
+                }
+
+                long[] d = stack.ToArray();
+                Array.Reverse(d);
+
+                long result = Convert.ToInt64(string.Concat(d));
+                totalOutputJoltage += result;
+            }
+
+            Console.WriteLine("Result: " + totalOutputJoltage);
+            Debug.Assert(totalOutputJoltage == 173848577117276, "Expected result to be 173848577117276");
+        }
+    }
+}
